39a6e9f8cfbfa3765a0edb27a8c08789356b0625
Vemas Bagus <masbrovemas@gmail.com>
2025-02-17 14:27:19 +0700

    praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function (){
+    return 'Hello World';
+});
+
+Route::get('/world', function (){
+    return 'World';
+});
+
+Route::get('/', function (){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function (){
+    return '2341720137 Vemas Bagus Fermanda';
+});
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama Saya '.$name;
+// });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//         return 'Nama saya '.$name;
+// });
+
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+//     });SS
+
+Route::get('/user/profile', function() {
+        return 'love you';
+       })->name('profile');
